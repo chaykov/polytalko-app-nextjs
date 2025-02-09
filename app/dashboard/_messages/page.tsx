7b1ecf435194a87789e0353fc00f5643ed08096b
chaykov <toumarenshi@gmail.com>
2025-02-09 21:47:29 +0100

    fixed 2 again

--- app/dashboard/_messages/page.tsx
+++ app/dashboard/_messages/page.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import SendMessage from "../_components/SendMessage";
-import GetMessages from "../_components/GetMessages";
 
 export default function Messages() {
-  return (
-    <div>
-      <SendMessage />
-      <GetMessages />
-    </div>
-  );
+  return <div>Messages</div>;
 }
